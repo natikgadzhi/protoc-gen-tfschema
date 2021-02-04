65a750c97345190d55bc07f3aa4a156f67dccf08
Victor Sokolov <gzigzigzeo@gmail.com>
2021-02-04 16:53:44 +0400

    Added some comments

--- reducer/main.go
+++ reducer/main.go
@@ -26,21 +26,24 @@ import (
 func Reduce(m builder.ResourceMap) {
 
 	for name, value := range m {
+		// Resource has a signle field
 		if len(value.Schema) == 1 {
 			keys := value.Schema.Keys()
 			replacementValue := value.Schema[keys[0]]
 
 			replace(m, name, replacementValue)
-			delete(m, name)
+			delete(m, name) // Reference to this resource is no longer needed
 		}
 	}
 }
 
 // Scans all resources and replaces reference to nested resource with single nested field itself
-func replace(m builder.ResourceMap, replacementName string, replacementValue *builder.Schema) {
+func replace(m builder.ResourceMap, nameToReplace string, replacementValue *builder.Schema) {
 	for _, resource := range m {
 		for name, schema := range resource.Schema {
-			if schema.Elem != nil && schema.Elem == replacementName {
+			// This schema field references nested resource in question
+			if schema.Elem != nil && schema.Elem == nameToReplace {
+				// Replace it with nested field definition
 				resource.Schema[name] = replacementValue
 			}
 		}
